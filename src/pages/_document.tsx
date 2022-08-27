fdb6c5a5db9fcf9b5ec4d455f46fa622ce87b30e
Abraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2022-08-27 12:37:45 +0100

    feat: Add icons, favicons & page title & desc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,22 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/logoIcons/udomy-favicon.svg" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="theme-color" content="#000000" />
+          <meta name="description" content="Course player environment" />
+
+          <link rel="apple-touch-icon" href="/logo192.png" />
+          <link rel="manifest" href="/manifest.json" />
+
+          <title>Udomy - Course player</title>
+        </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
@@ -13,6 +24,6 @@ export default class Document extends NextDocument {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
