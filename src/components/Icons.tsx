1eadb0896ec6da90f5042af2ce7ca2be16a1f793
Abraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2022-08-28 15:39:47 +0100

    refactor: update Icon viewports  (24 24), & change PlayIcon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -2,7 +2,7 @@ import { createIcon } from "@chakra-ui/icons";
 
 export const ArchiveIcon = createIcon({
   displayName: "ArchiveIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M4 20C3.45 20 2.97933 19.8043 2.588 19.413C2.196 19.021 2 18.55 2 18V6C2 5.45 2.196 4.97933 2.588 4.588C2.97933 4.196 3.45 4 4 4H9.175C9.44167 4 9.696 4.05 9.938 4.15C10.1793 4.25 10.3917 4.39167 10.575 4.575L12 6H20C20.55 6 21.021 6.196 21.413 6.588C21.8043 6.97933 22 7.45 22 8V18C22 18.55 21.8043 19.021 21.413 19.413C21.021 19.8043 20.55 20 20 20H4Z"
@@ -13,7 +13,7 @@ export const ArchiveIcon = createIcon({
 
 export const ArrowLeftIcon = createIcon({
   displayName: "ArrowLeftIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z"
@@ -24,7 +24,7 @@ export const ArrowLeftIcon = createIcon({
 
 export const CaptionOffIcon = createIcon({
   displayName: "CaptionOffIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M6.83 4H19C20.1 4 21 4.9 21 6V18C21 18.05 20.99 18.1 20.98 18.16L17.6 14.78C17.84 14.59 18 14.32 18 14V13H16.5V13.5H16.33L14.5 11.67V10.5H16.5V11H18V10C18 9.45 17.55 9 17 9H14C13.45 9 13 9.45 13 10V10.17L6.83 4ZM19.78 22.61L17.17 20H5C4.46957 20 3.96086 19.7893 3.58579 19.4142C3.21071 19.0391 3 18.5304 3 18V6C3 5.95 3.02 5.9 3.02 5.85L1.39 4.22L2.8 2.81L21.18 21.19L19.78 22.61ZM11 13.83L10.17 13H9.5V13.5H7.5V10.5H7.67L6.4 9.22C6.16 9.41 6 9.68 6 10V14C6 14.55 6.45 15 7 15H10C10.55 15 11 14.55 11 14V13.83Z"
@@ -35,7 +35,7 @@ export const CaptionOffIcon = createIcon({
 
 export const CaptionIcon = createIcon({
   displayName: "CaptionIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M19 4H5C4.46957 4 3.96086 4.21071 3.58579 4.58579C3.21071 4.96086 3 5.46957 3 6V18C3 18.5304 3.21071 19.0391 3.58579 19.4142C3.96086 19.7893 4.46957 20 5 20H19C20.1 20 21 19.1 21 18V6C21 4.9 20.1 4 19 4ZM11 11H9.5V10.5H7.5V13.5H9.5V13H11V14C11 14.55 10.55 15 10 15H7C6.45 15 6 14.55 6 14V10C6 9.45 6.45 9 7 9H10C10.55 9 11 9.45 11 10V11ZM18 11H16.5V10.5H14.5V13.5H16.5V13H18V14C18 14.55 17.55 15 17 15H14C13.45 15 13 14.55 13 14V10C13 9.45 13.45 9 14 9H17C17.55 9 18 9.45 18 10V11Z"
@@ -46,13 +46,13 @@ export const CaptionIcon = createIcon({
 
 export const CheckboxBlankIcon = createIcon({
   displayName: "CheckboxBlankIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: <path fill="currentColor" d="M19 5v14H5V5h14m2-2H3v18h18V3z" />,
 });
 
 export const CheckboxIcon = createIcon({
   displayName: "CheckboxIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       fill="currentColor"
@@ -63,7 +63,7 @@ export const CheckboxIcon = createIcon({
 
 export const ChevronDownIcon = createIcon({
   displayName: "ChevronDownIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M16.293 9.29303L12 13.586L7.707 9.29303L6.293 10.707L12 16.414L17.707 10.707L16.293 9.29303Z"
@@ -74,7 +74,7 @@ export const ChevronDownIcon = createIcon({
 
 export const ContractIcon = createIcon({
   displayName: "ContractIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: [
     <path
       d="M23.1924 10.9527L17.8314 10.9527L20.3699 8.41422L18.9557 7.00001L14.006 11.9498L18.9557 16.8995L20.3699 15.4853L17.8314 12.9468L23.1924 12.9468V10.9527Z"
@@ -89,7 +89,7 @@ export const ContractIcon = createIcon({
 
 export const DocumentIcon = createIcon({
   displayName: "DocumentIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M13 9H18.5L13 3.5V9ZM6 2H14L20 8V20C20 20.5304 19.7893 21.0391 19.4142 21.4142C19.0391 21.7893 18.5304 22 18 22H6C5.46957 22 4.96086 21.7893 4.58579 21.4142C4.21071 21.0391 4 20.5304 4 20V4C4 2.89 4.89 2 6 2ZM15 18V16H6V18H15ZM18 14V12H6V14H18Z"
@@ -100,7 +100,7 @@ export const DocumentIcon = createIcon({
 
 export const DownloadOffIcon = createIcon({
   displayName: "DownloadOffIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       fill="currentColor"
@@ -111,7 +111,7 @@ export const DownloadOffIcon = createIcon({
 
 export const DownloadIcon = createIcon({
   displayName: "DownloadIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       fill="currentColor"
@@ -122,7 +122,7 @@ export const DownloadIcon = createIcon({
 
 export const EllipsisIcon = createIcon({
   displayName: "EllipsisIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       fill="currentColor"
@@ -133,7 +133,7 @@ export const EllipsisIcon = createIcon({
 
 export const ExpandIcon = createIcon({
   displayName: "ExpandIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: [
     <path
       d="M14 12.9468L19.361 12.9468L16.8225 15.4853L18.2367 16.8995L23.1864 11.9498L18.2367 7.00002L16.8225 8.41424L19.361 10.9527L14 10.9527V12.9468Z"
@@ -148,7 +148,7 @@ export const ExpandIcon = createIcon({
 
 export const FolderIcon = createIcon({
   displayName: "FolderIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M19 20H4C3.46957 20 2.96086 19.7893 2.58579 19.4142C2.21071 19.0391 2 18.5304 2 18V6C2 4.89 2.89 4 4 4H10L12 6H19C19.5304 6 20.0391 6.21071 20.4142 6.58579C20.7893 6.96086 21 7.46957 21 8H4V18L6.14 10H23.21L20.93 18.5C20.7 19.37 19.92 20 19 20Z"
@@ -159,7 +159,7 @@ export const FolderIcon = createIcon({
 
 export const FullscreenExitIcon = createIcon({
   displayName: "FullscreenExitIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: [
     <path
       d="M19.59 3L15.09 7.5V3.91H13.09V10.91H20.09V8.91H16.5L21 4.41L19.59 3Z"
@@ -174,7 +174,7 @@ export const FullscreenExitIcon = createIcon({
 
 export const FullscreenIcon = createIcon({
   displayName: "FullscreenIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M10 21V19H6.41L10.91 14.5L9.5 13.09L5 17.59V14H3V21H10ZM14.5 10.91L19 6.41V10H21V3H14V5H17.59L13.09 9.5L14.5 10.91Z"
@@ -185,7 +185,7 @@ export const FullscreenIcon = createIcon({
 
 export const NoteIcon = createIcon({
   displayName: "NoteIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M3 10H14V12H3V10ZM3 8H14V6H3V8ZM3 16H10V14H3V16ZM18.01 12.87L18.72 12.16C18.8125 12.0673 18.9224 11.9937 19.0434 11.9436C19.1643 11.8934 19.294 11.8676 19.425 11.8676C19.556 11.8676 19.6857 11.8934 19.8066 11.9436C19.9276 11.9937 20.0375 12.0673 20.13 12.16L20.84 12.87C21.23 13.26 21.23 13.89 20.84 14.28L20.13 14.99L18.01 12.87ZM17.3 13.58L12 18.88V21H14.12L19.42 15.7L17.3 13.58V13.58Z"
@@ -207,13 +207,13 @@ export const PageIcon = createIcon({
 
 export const PauseIcon = createIcon({
   displayName: "PauseIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: <path fill="currentColor" d="M6 19h4V5H6v14zm8-14v14h4V5h-4z" />,
 });
 
 export const PenIcon = createIcon({
   displayName: "PenIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: [
     <path
       d="M2.81332 15.4667L1.54665 20.9333C1.50296 21.1332 1.50445 21.3402 1.55104 21.5394C1.59762 21.7386 1.68811 21.9249 1.81589 22.0846C1.94368 22.2443 2.10554 22.3735 2.28964 22.4627C2.47374 22.5518 2.67543 22.5988 2.87999 22.6C2.9753 22.6096 3.07134 22.6096 3.16665 22.6L8.66665 21.3333L19.2267 10.8133L13.3333 4.93332L2.81332 15.4667Z"
@@ -228,7 +228,7 @@ export const PenIcon = createIcon({
 
 export const PictureInPictureExitIcon = createIcon({
   displayName: "PictureInPictureExitIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M21 3C21.2652 3 21.5196 3.10536 21.7071 3.29289C21.8946 3.48043 22 3.73478 22 4V11H20V5H4V19H10V21H3C2.73478 21 2.48043 20.8946 2.29289 20.7071C2.10536 20.5196 2 20.2652 2 20V4C2 3.73478 2.10536 3.48043 2.29289 3.29289C2.48043 3.10536 2.73478 3 3 3H21ZM21 13C21.2652 13 21.5196 13.1054 21.7071 13.2929C21.8946 13.4804 22 13.7348 22 14V20C22 20.2652 21.8946 20.5196 21.7071 20.7071C21.5196 20.8946 21.2652 21 21 21H13C12.7348 21 12.4804 20.8946 12.2929 20.7071C12.1054 20.5196 12 20.2652 12 20V14C12 13.7348 12.1054 13.4804 12.2929 13.2929C12.4804 13.1054 12.7348 13 13 13H21ZM11.5 7L9.457 9.043L11.707 11.293L10.293 12.707L8.043 10.457L6 12.5V7H11.5Z"
@@ -239,7 +239,7 @@ export const PictureInPictureExitIcon = createIcon({
 
 export const PictureInPictureIcon = createIcon({
   displayName: "PictureInPictureIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M21 3C21.2652 3 21.5196 3.10536 21.7071 3.29289C21.8946 3.48043 22 3.73478 22 4V11H20V5H4V19H10V21H3C2.73478 21 2.48043 20.8946 2.29289 20.7071C2.10536 20.5196 2 20.2652 2 20V4C2 3.73478 2.10536 3.48043 2.29289 3.29289C2.48043 3.10536 2.73478 3 3 3H21ZM21 13C21.2652 13 21.5196 13.1054 21.7071 13.2929C21.8946 13.4804 22 13.7348 22 14V20C22 20.2652 21.8946 20.5196 21.7071 20.7071C21.5196 20.8946 21.2652 21 21 21H13C12.7348 21 12.4804 20.8946 12.2929 20.7071C12.1054 20.5196 12 20.2652 12 20V14C12 13.7348 12.1054 13.4804 12.2929 13.2929C12.4804 13.1054 12.7348 13 13 13H21ZM6.707 6.293L8.957 8.543L11 6.5V12H5.5L7.543 9.957L5.293 7.707L6.707 6.293V6.293Z"
@@ -250,13 +250,13 @@ export const PictureInPictureIcon = createIcon({
 
 export const PlayIcon = createIcon({
   displayName: "PlayIcon",
-  viewBox: "0 0 200 200",
-  path: <path fill="currentColor" d="m96 448l320-192L96 64v384z" />,
+  viewBox: "0 0 24 24",
+  path: <path fill="currentColor" d="m8 19l11-7L8 5v14Z" />,
 });
 
 export const RewindIcon = createIcon({
   displayName: "RewindIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M7 6L12 11V7C15.31 7 18 9.69 18 13C18 16.31 15.31 19 12 19C8.69 19 6 16.31 6 13H4C4 17.42 7.58 21 12 21C16.42 21 20 17.42 20 13C20 8.58 16.42 5 12 5V1L7 6Z"
@@ -267,7 +267,7 @@ export const RewindIcon = createIcon({
 
 export const SharpStarIcon = createIcon({
   displayName: "StarIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M12 1.20001L15.6 8.40001L22.8 9.30001L17.856 14.844L19.2 22.8L12 19.2L4.8 22.8L6.156 14.844L1.2 9.30001L8.4 8.40001L12 1.20001Z"
@@ -278,7 +278,7 @@ export const SharpStarIcon = createIcon({
 
 export const TrophyIcon = createIcon({
   displayName: "TrophyIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M20.3438 3.75H18.1875V2.8125C18.1875 2.70937 18.1031 2.625 18 2.625H6C5.89687 2.625 5.8125 2.70937 5.8125 2.8125V3.75H3.65625C3.38275 3.75 3.12044 3.85865 2.92705 4.05205C2.73365 4.24544 2.625 4.50775 2.625 4.78125V8.25C2.625 10.1648 4.03125 11.7563 5.86406 12.0469C6.22734 14.7703 8.41406 16.9148 11.1562 17.2148V19.6805H6.5625C6.14766 19.6805 5.8125 20.0156 5.8125 20.4305V21.1875C5.8125 21.2906 5.89687 21.375 6 21.375H18C18.1031 21.375 18.1875 21.2906 18.1875 21.1875V20.4305C18.1875 20.0156 17.8523 19.6805 17.4375 19.6805H12.8438V17.2148C15.5859 16.9148 17.7727 14.7703 18.1359 12.0469C19.9687 11.7563 21.375 10.1648 21.375 8.25V4.78125C21.375 4.50775 21.2664 4.24544 21.073 4.05205C20.8796 3.85865 20.6173 3.75 20.3438 3.75ZM4.3125 8.25V5.4375H5.8125V10.3031C5.37714 10.1641 4.99723 9.89025 4.72762 9.52122C4.45801 9.15219 4.31264 8.70703 4.3125 8.25V8.25ZM16.5 11.25C16.5 12.4008 16.0523 13.4859 15.2367 14.2992C14.4211 15.1148 13.3383 15.5625 12.1875 15.5625H11.8125C10.6617 15.5625 9.57656 15.1148 8.76328 14.2992C7.94766 13.4836 7.5 12.4008 7.5 11.25V4.3125H16.5V11.25ZM19.6875 8.25C19.6875 9.21094 19.057 10.0266 18.1875 10.3031V5.4375H19.6875V8.25Z"
@@ -300,7 +300,7 @@ export const VideoIcon = createIcon({
 
 export const VolumeHighIcon = createIcon({
   displayName: "VolumeHighIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M3 9.00001V15H7L12 20V4.00001L7 9.00001H3ZM16.5 12C16.4998 11.1621 16.2657 10.341 15.824 9.62895C15.3823 8.91695 14.7506 8.3424 14 7.97001V16.02C15.48 15.29 16.5 13.77 16.5 12ZM14 3.23001V5.29001C16.89 6.15001 19 8.83001 19 12C19 15.17 16.89 17.85 14 18.71V20.77C18.01 19.86 21 16.28 21 12C21 7.72001 18.01 4.14001 14 3.23001V3.23001Z"
@@ -311,7 +311,7 @@ export const VolumeHighIcon = createIcon({
 
 export const VolumeLowIcon = createIcon({
   displayName: "VolumeLowIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M18.5 12C18.4998 11.1621 18.2657 10.3409 17.824 9.62894C17.3823 8.91694 16.7506 8.34239 16 7.97V16.02C17.48 15.29 18.5 13.77 18.5 12ZM5 9V15H9L14 20V4L9 9H5Z"
@@ -322,7 +322,7 @@ export const VolumeLowIcon = createIcon({
 
 export const VolumeOffIcon = createIcon({
   displayName: "VolumeOffIcon",
-  viewBox: "0 0 200 200",
+  viewBox: "0 0 24 24",
   path: (
     <path
       d="M16.5 12C16.4998 11.1621 16.2657 10.3409 15.824 9.62894C15.3823 8.91694 14.7506 8.34239 14 7.97V10.18L16.45 12.63C16.48 12.43 16.5 12.22 16.5 12V12ZM19 12C19 12.94 18.8 13.82 18.46 14.64L19.97 16.15C20.6495 14.8724 21.0032 13.447 21 12C21 7.72 18.01 4.14 14 3.23V5.29C16.89 6.15 19 8.83 19 12ZM4.27 3L3 4.27L7.73 9H3V15H7L12 20V13.27L16.25 17.52C15.58 18.04 14.83 18.45 14 18.7V20.76C15.3533 20.4497 16.6163 19.8302 17.69 18.95L19.73 21L21 19.73L12 10.73L4.27 3ZM12 4L9.91 6.09L12 8.18V4Z"
