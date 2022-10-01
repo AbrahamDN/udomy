aa03283ce674db16dc59e98d350f9aa400dcf131
Abraham Ndinga <40493309+AbrahamDN@users.noreply.github.com>
2022-10-01 04:32:08 +0100

    refactor: add VideoControlSpeed to VideoControls

--- src/components/Video/VideoControls/index.tsx
+++ src/components/Video/VideoControls/index.tsx
@@ -1,17 +1,6 @@
 import React, { useContext } from "react";
-// import { useEvent } from "react-use";
 
-import {
-  Button,
-  Container,
-  Divider,
-  Flex,
-  Menu,
-  MenuButton,
-  MenuList,
-  Text,
-  Tooltip,
-} from "@chakra-ui/react";
+import { Container, Divider, Flex, Text } from "@chakra-ui/react";
 import VideoContext from "../../../../context/video.context";
 import { PauseIcon, PlayIcon, RewindIcon } from "../../Icons";
 import {
@@ -21,10 +10,10 @@ import {
   useVideoRate,
 } from "../../../globalStates";
 import VideoControlButton from "./VideoControlButton";
-import VideoRateItem from "./VideoRateItem";
 import formatDuration from "../utils/formatDuration";
 import VideoControlVolume from "./VideoControlVolume";
 import VideoControlCaption from "./VideoControlCaption";
+import VideoControlSpeed from "./VideoControlSpeed";
 
 const VideoControls = () => {
   const {
@@ -33,7 +22,6 @@ const VideoControls = () => {
   } = useContext(VideoContext);
   const setHoverActive = useVideoHoverActive()[1];
   const [paused] = usePaused();
-  const [speedRate] = useVideoRate();
   const [currentTime] = useVideoCurrentTime();
 
   const video = videoRef.current;
@@ -61,43 +49,7 @@ const VideoControls = () => {
           <RewindIcon w="6" h="6" />
         </VideoControlButton>
 
-        <Menu>
-          <Tooltip label="Playback rate" placement="top" p="2" mb="4">
-            <MenuButton
-              as={Button}
-              h="fit-content"
-              w="20"
-              px="1"
-              borderRadius="0"
-              bgColor="white"
-              color="black"
-              fontWeight="bold"
-              fontSize="sm"
-              _hover={{ bgColor: "white" }}
-              _focus={{ bgColor: "white" }}
-            >
-              {speedRate}x
-            </MenuButton>
-          </Tooltip>
-
-          <MenuList
-            color="white"
-            bgColor="black"
-            borderRadius="0"
-            border="1px solid"
-            borderColor="whiteAlpha.500"
-            fontSize="sm"
-            mb="6"
-            minW="min-content"
-          >
-            <VideoRateItem rate={0.5} />
-            <VideoRateItem rate={0.75} />
-            <VideoRateItem rate={1} />
-            <VideoRateItem rate={1.25} />
-            <VideoRateItem rate={1.75} />
-            <VideoRateItem rate={2} />
-          </MenuList>
-        </Menu>
+        <VideoControlSpeed />
 
         <VideoControlButton onClick={() => skip(5)}>
           <RewindIcon w="6" h="6" transform="scaleX(-1)" />
